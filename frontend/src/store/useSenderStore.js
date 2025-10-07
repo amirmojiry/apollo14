dfef507176b1dc35dfbb6123fde665a2f0ce52fd
Amir Mojiri <amojiry@gmail.com>
2025-10-07 15:28:47 +0300

    Fix the finalize bug

--- frontend/src/store/useSenderStore.js
+++ frontend/src/store/useSenderStore.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-export const useSenderStore = create((set) => ({
+export const useSenderStore = create((set, get) => ({
   image: null,
   location: null,
   barValue: 5,
@@ -10,24 +10,25 @@ export const useSenderStore = create((set) => ({
   setBarValue: (barValue) => set({ barValue }),
 
   sendData: async () => {
-    set((state) => {
-      const payload = {
-        image: state.image,
-        barValue: state.barValue,
-        latitude: state.location?.latitude,
-        longitude: state.location?.longitude,
-      };
+    const state = get();
+    const payload = {
+      image: state.image,
+      barValue: state.barValue,
+      latitude: state.location?.latitude,
+      longitude: state.location?.longitude,
+    };
 
-      console.log(" Sending payload:", payload);
-
-      fetch("http://localhost:4000/api/upload", {
+    console.log(" Sending payload:", payload);
+    try {
+      const res = await fetch("http://localhost:4000/api/upload", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
-      })
-        .then((res) => res.json())
-        .then((data) => console.log(" Backend response:", data))
-        .catch((err) => console.error(" Error sending data:", err));
-    });
+      });
+      const data = await res.json().catch(() => null);
+      console.log(" Backend response:", data);
+    } catch (err) {
+      console.error(" Error sending data:", err);
+    }
   },
 }));
